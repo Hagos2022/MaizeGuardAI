3ea58e2f3d596ea692099597bf3cd08465cb4e21
FatimaHarunaImam <101684307+FatimaHarunaImam@users.noreply.github.com>
2025-01-06 16:40:46 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Include React plugin for Vite
   plugins: [react()],
+  
   optimizeDeps: {
+    // Exclude 'lucide-react' from dependency pre-bundling
     exclude: ['lucide-react'],
   },
 });
